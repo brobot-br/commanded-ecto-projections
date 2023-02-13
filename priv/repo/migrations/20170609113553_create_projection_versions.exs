5307eb07805016815e33fde2b860b6edc20792ea
y86 <yuri.de.figueiredo@gmail.com>
2023-02-13 13:11:30 -0300

    Update tests cases

--- priv/repo/migrations/20170609113553_create_projection_versions.exs
+++ priv/repo/migrations/20170609113553_create_projection_versions.exs
@@ -4,6 +4,7 @@ defmodule Commanded.Projections.Repo.Migrations.CreateProjectionVersions do
   def change do
     create table(:projection_versions, primary_key: false) do
       add(:projection_name, :text, primary_key: true)
+      add(:projection_partition_key, :text, primary_key: true)
       add(:last_seen_event_number, :bigint)
 
       timestamps(type: :naive_datetime_usec)
--- test/support/projection_assertions.ex
+++ test/support/projection_assertions.ex
@@ -9,15 +9,18 @@ defmodule Commanded.Projections.ProjectionAssertions do
     assert actual == expected
   end
 
-  def assert_seen_event(projection_name, expected_last_seen)
-      when is_binary(projection_name) and is_integer(expected_last_seen) do
-    assert last_seen_event(projection_name) == expected_last_seen
+  def assert_seen_event(projection_name, projection_partition_key \\ "", expected_last_seen)
+      when is_binary(projection_name) and is_binary(projection_partition_key) and
+             is_integer(expected_last_seen) do
+    assert last_seen_event(projection_name, projection_partition_key) == expected_last_seen
   end
 
-  def last_seen_event(projection_name) when is_binary(projection_name) do
-    sql = "SELECT last_seen_event_number from projection_versions where projection_name = $1"
+  def last_seen_event(projection_name, projection_partition_key \\ "")
+      when is_binary(projection_name) and is_binary(projection_partition_key) do
+    sql =
+      "SELECT last_seen_event_number from projection_versions where projection_name = $1 and projection_partition_key = $2"
 
-    case Ecto.Adapters.SQL.query(Repo, sql, [projection_name]) do
+    case Ecto.Adapters.SQL.query(Repo, sql, [projection_name, projection_partition_key]) do
       {:ok, %{num_rows: 0}} -> nil
       {:ok, %{rows: [[last_seen]], num_rows: 1}} -> last_seen
     end
